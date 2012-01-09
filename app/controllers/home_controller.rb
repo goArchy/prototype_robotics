9e397bf90340d97bf1a8da4ef6a2d95f313c4175
JD_home <johndavid400@yahoo.com>
2012-01-08 23:13:31 -0600

    [HOME]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @posts = Post.all
     @featured_posts = Post.all.select{|p| p.featured?}
+    @projects = Project.all
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, :styles => { :large => "640x480!", :slider => "460x345!", :medium => "320x240!", :thumb => "100x100!" }
+  has_attached_file :image, :styles => { :large => "640x480!", :slider => "480x360!", :medium => "320x240!", :thumb => "100x100!" }
 end
