989bfddc68fdb123a663331c227d73375efc5cea
johndavid400 <johndavid400@yahoo.com>
2012-01-23 14:48:37 -0600

    [DB RESET]

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(:version => 20120109025136) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
-  create_table "posts", :force => true do |t|
+  create_table "articles", :force => true do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "user_id"
