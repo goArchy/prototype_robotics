cbf03347bfd0019d3acb426c26ff35bc95071d31
JD_home <johndavid400@yahoo.com>
2012-02-20 15:27:19 -0600

    env vars

---
+++ config/initializers/spree_analytics.rb
@@ -0,0 +1,4 @@
+SpreeAnalytics.app_id = password = ENV['SPREE_ANALYTICS_APP_ID']
+SpreeAnalytics.site_id = password = ENV['SPREE_ANALYTICS_SITE_ID']
+SpreeAnalytics.token = password = ENV['SPREE_ANALYTICS_TOKEN']
+
