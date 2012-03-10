5df7c2cfa2dcbbeb742427aef0333ebdf31e6b3c
JD_home <johndavid400@yahoo.com>
2012-03-10 10:17:04 -0600

    image sizes

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,7 +1,7 @@
 class Tutorial < ActiveRecord::Base
   has_many :steps
   accepts_nested_attributes_for :steps
-  has_attached_file :image, :styles => { :large => "640x480!", :slider => "480x360!", :medium => "320x240!", :thumb => "100x100!" }
+  has_attached_file :image, :styles => { :large => "640x480>", :slider => "480x360>", :medium => "320x240>", :thumb => "100x100>" }
   include PgSearch
   multisearchable :against => [:name, :description]
   has_many :galleries
