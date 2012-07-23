c3ca530a058b4d2c4ba21e5810ef38799a2f3fb2
johndavid400 <jd@isotope11.com>
2012-07-23 11:49:18 -0500

    adding user project creation

--- app/overrides/user_overrides.rb
+++ app/overrides/user_overrides.rb
@@ -1,6 +1,9 @@
-
 Deface::Override.new(:virtual_path      => "spree/admin/users/_form",
                      :name              => "admin_users_form",
                      :insert_bottom     => "[data-hook='admin_user_form_fields']",
                      :partial           => "shared/user_form")
-                     
+
+Deface::Override.new(:virtual_path      => "spree/users/show",
+                     :name              => "users_profile_add_projects",
+                     :insert_after     => "[data-hook='account_my_orders']",
+                     :partial           => "shared/user_projects")
