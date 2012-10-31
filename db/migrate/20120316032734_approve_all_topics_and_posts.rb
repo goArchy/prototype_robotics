73487006cdb6bc456283a2323640ebb7e760e3ef
johndavid400 <jd@isotope11.com>
2012-10-31 16:35:31 -0500

    remove all forem migrations

--- db/migrate/20120316032734_approve_all_topics_and_posts.rb
+++ db/migrate/20120316032734_approve_all_topics_and_posts.rb
@@ -1,7 +1,7 @@
 class ApproveAllTopicsAndPosts < ActiveRecord::Migration
   def up
-    Forem::Topic.update_all :state => "approved"
-    Forem::Post.update_all :state => "approved"
+#    Forem::Topic.update_all :state => "approved"
+#    Forem::Post.update_all :state => "approved"
   end
 
   def down
--- db/migrate/20120316032737_add_forem_state.rb
+++
@@ -1,6 +0,0 @@
-class AddForemState < ActiveRecord::Migration
-
-  def change
-    add_column :spree_users, :forem_state, :string, :default => 'pending_review'
-  end
-end
