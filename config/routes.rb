874d6ace4a715690b1f671b53ee332af308ec2f3
johndavid400 <jd@isotope11.com>
2012-10-31 16:49:16 -0500

    removing gem

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,26 @@ PrototypeRobotics::Application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   mount Spree::Core::Engine, :at => '/'
 
+
+  namespace :admin do
+    resources :projects
+    resources :tutorials
+    resources :steps
+    resources :articles
+    resources :galleries
+    match "remove_image" => "galleries#remove_image"
+  end
+
+  resources :projects
+  resources :tutorials
+  resources :steps
+  resources :articles, :path => "blog"
+
+  match "search_projects" => "projects#search_projects"
+  match "search" => "home#search"
+
+  match "about_me" => "home#about_me"
+  match "links" => "home#links"
+
 end
 
