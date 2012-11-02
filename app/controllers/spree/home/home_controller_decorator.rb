a25a8b372bbf061b8762463fb965c587bfed4bc5
johndavid400 <jd@isotope11.com>
2012-11-02 17:13:31 -0500

    updating

--- app/controllers/spree/home/home_controller_decorator.rb
+++ app/controllers/spree/home/home_controller_decorator.rb
@@ -9,16 +9,16 @@ Spree::HomeController.class_eval do
   end
 
   def add_vars
-    @featured_content = Spree::Article.all.select{|a| a.featured? && a.published?}
-    @featured_projects = Spree::Project.all.select{|p| p.featured? && p.published?}
+    @featured_content = Article.all.select{|a| a.featured? && a.published?}
+    @featured_projects = Project.all.select{|p| p.featured? && p.published?}
     if !@featured_projects.empty?
       @featured_projects.each do |project|
         @featured_content.push(project)
       end
     end
-    @articles = Spree::Article.where(:published => "true").last(3)
-    @projects = Spree::Project.where(:published => "true").last(3)
-    @tutorials = Spree::Tutorial.all.last(3)
+    @articles = Article.where(:published => "true").last(3)
+    @projects = Project.where(:published => "true").last(3)
+    @tutorials = Tutorial.all.last(3)
   end
 
   def search
@@ -27,18 +27,18 @@ Spree::HomeController.class_eval do
       @search_results = PgSearch.multisearch(@search)
 
       @projects = []
-      @search_results.select{|p| p.searchable_type == "Spree::Project"}.each do |project|
-        @projects.push(Spree::Project.find(project.searchable_id))
+      @search_results.select{|p| p.searchable_type == "Project"}.each do |project|
+        @projects.push(Project.find(project.searchable_id))
       end
 
       @articles = []
-      @search_results.select{|p| p.searchable_type == "Spree::Article"}.each do |article|
-        @articles.push(Spree::Article.find(article.searchable_id))
+      @search_results.select{|p| p.searchable_type == "Article"}.each do |article|
+        @articles.push(Article.find(article.searchable_id))
       end
 
       @tutorials = []
-      @search_results.select{|p| p.searchable_type == "Spree::Tutorial"}.each do |tutorial|
-        @tutorials.push(Spree::Tutorial.find(tutorial.searchable_id))
+      @search_results.select{|p| p.searchable_type == "Tutorial"}.each do |tutorial|
+        @tutorials.push(Tutorial.find(tutorial.searchable_id))
       end
       render 'home/search'
     else
