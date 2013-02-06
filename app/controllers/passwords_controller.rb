8840d48b9543f70ddc5221d0c26cf289d4902372
johndavid400 <jd@isotope11.com>
2013-02-06 09:29:54 -0600

    add password change form on dashboard

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,15 +2,23 @@ class PasswordsController < ApplicationController
   before_filter :verify
 
   def change_password
-    @foo = "foo"
-    binding.pry
+    @user.password = params[:password]
+    @user.password_confirmation = params[:password_confirmation]
+    if @user.save
+      flash[:notice] = "Your password has been successfully changed."
+    else
+      flash[:alert] = "There was an error changing your password, please try again."
+    end
+    redirect_to dashboard_index_path
   end
 
   private
 
   def verify
-    @foo = "bar"
-    binding.pry
+    @user = User.find(params[:user_id])
+  rescue
+    flash[:alert] = "You must log in to do that"
+    redirect_to root_path
   end
 
 end
