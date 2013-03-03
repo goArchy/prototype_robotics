b30205924735cda74d5c39c20779d81fd415cb27
johndavid400 <johndavid400@gmail.com>
2013-03-03 10:42:09 -0600

    change ordering of homepage content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,9 +19,9 @@ class HomeController < ApplicationController
         @featured_content.push(project)
       end
     end
-    @articles = Article.published.order("created_at ASC").last(3)
-    @projects = Project.published.order("created_at ASC").last(3)
-    @tutorials = Tutorial.order("created_at ASC").all.last(3)
+    @articles = Article.published.order("created_at ASC").last(3).reverse
+    @projects = Project.published.order("created_at ASC").last(3).reverse
+    @tutorials = Tutorial.order("created_at ASC").all.last(3).reverse
   end
 
   def search
