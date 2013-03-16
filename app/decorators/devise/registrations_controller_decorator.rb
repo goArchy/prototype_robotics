9da046e643f93345eb3d17a4f184df584f9e25c8
johndavid400 <johndavid400@gmail.com>
2013-03-16 11:21:24 -0500

    fix

--- app/decorators/devise/registrations_controller_decorator.rb
+++ app/decorators/devise/registrations_controller_decorator.rb
@@ -2,7 +2,7 @@ Devise::RegistrationsController.class_eval do
   after_filter :approve_forem, :only => [:create]
 
   def approve_forem
-    @user.notifications.create(message: "#{@user.name.username} just registered.")
+    @user.notifications.create(message: "#{@user.username} just registered.")
     @user.forem_state = "approved"
     @user.save
   end
